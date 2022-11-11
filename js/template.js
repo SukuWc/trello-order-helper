c2a8fcffc20f88aa2edeb3f4d1be939649b5f67c
suku <sukuwc@riseup.net>
2022-11-11 13:00:55 +0100

    SUKU list debug

--- js/template.js
+++ js/template.js
@@ -47,7 +47,12 @@ TrelloPowerUp.initialize({
           });
 
           return t.cards("all").then(function (cards) {
-            console.log(JSON.stringify(cards, null, 2));
+            cards.forEach(element => {
+              console.log(element.idList)
+              if (element.idList === list.id){
+                console.log(element.name)
+              }
+            });
           });
           
         }
